b43073d8361763aa8ed22c50cdb11a18942c997a
vikram <vsingh26@buffalo.edu>
2019-05-03 13:37:55 -0400

    updated project

--- nbFunctions.py
+++ nbFunctions.py
@@ -12,19 +12,20 @@ class NBC(BaseEstimator):
     '''
 
     def __init__(self, a=1, b=1, alpha=1):
-        self.__a = a
-        self.__b = b
-        self.__alpha = alpha
-        self.__params = None
+        self.a = a
+        self.b = b
+        self.alpha = alpha
+        self.params = None
+
         
     def get_a(self):
-        return self.__a
+        return self.a
 
     def get_b(self):
-        return self.__b
+        return self.b
 
     def get_alpha(self):
-        return self.__alpha
+        return self.alpha
 
     # you need to implement this function
 
@@ -41,11 +42,57 @@ class NBC(BaseEstimator):
         alpha = self.get_alpha()
         self.__classes = np.unique(y)
 
+        #####equation 5
+        N1= 0
+
+        for i in range(0,y.shape[0]):
+            if y[i]==1:
+                N1+=1
+        theta= (N1+ a)/(y.shape[0]+ a + b)
+        ##print("/",theta)
+        #################
+        #print(np.unique(X[0]))
+        #####equation 8
+        theta1= np.zeros((X.shape[1],1))
+        K= np.zeros((18,1))
+
+        for j in range(0,X.shape[1]):
+            Attr= X.T[j]
+            k= np.unique(Attr).size
+            K[j]=k
+        N1j=0
+        """for j in range(0,X.shape[1]):
+            Attr= X.T[j]
+            k= np.unique(Attr).size
+            for i in range(0,X.shape[0]):
+                if (Attr[i] in range(1,K)) and y[i]==1:
+                    N1j+=1
+            theta1[j]=(N1j+ alpha)/(N1+ K*alpha)"""
+
+        ##################
+
+        ######equation 9
+
+        theta2= np.zeros((X.shape[1],1))
+
+        N2j=0
+        """for j in range(0,X.shape[1]):
+            Attr= X.T[j]
+            k= np.unique(Attr).size
+            for i in range(0,X.shape[0]):
+                if (Attr[i] in range(1,k)) and y[i]==2:
+                    N2j+=1
+            theta2[j]=(N1j+ alpha)/(y.shape[0]- N1+ K*alpha)"""
+         ##################
+
+
         # remove next line and implement from here
         # you are free to use any data structure for paramse
-        params = None 
+        params1= [theta,X,y,N1,K]
+        #params = None
+
         # do not change the line below
-        self.__params = params
+        self.params = params1
     
     # you need to implement this function
     def predict(self,Xtest):
@@ -58,13 +105,58 @@ class NBC(BaseEstimator):
         Output:
         predictions: N length numpy array containing the predictions
         '''
-        params = self.__params
+
+        params = self.params
         a = self.get_a()
         b = self.get_b()
         alpha = self.get_alpha()
         #remove next line and implement from here
+        m= params[0]* np.prod(params[1])
         predictions = np.random.choice(self.__classes,np.unique(Xtest.shape[0]))
         #do not change the line below
+        nMatrix1 = np.zeros(Xtest.shape)
+        nMatrix2 = np.zeros(Xtest.shape)
+        for i in range(0,len(Xtest)):
+            for j in range(0,18):
+                n1j = 0
+                n2j = 0
+                for k in range(0,len(params[1])):
+                    if(Xtest[i][j] == params[1][k][j] and params[2][k] == 1):
+                        n1j += 1
+                    if(Xtest[i][j] == params[1][k][j] and params[2][k] == 2):
+                        n2j += 1
+
+                theta1j = (n1j + alpha)/(params[3]+(params[4][j]))
+                theta2j =  (n2j + alpha)/((len(params[2]) - params[3])+(params[4][j]))
+                #temp_tuple = (theta1j, theta2j)
+                nMatrix1[i][j] = theta1j
+                nMatrix2[i][j] = theta2j
+                #inner_list.append(temp_tuple)
+
+
+
+
+        ypred = np.zeros((Xtest.shape[0],1))
+
+
+        temp1 = 1
+        temp2 = 1
+        for i in range(0,Xtest.shape[0]):
+            temp= nMatrix1[i]
+            prod= np.prod(temp)
+            a=params[0]*prod
+            temp= nMatrix2[i]
+            prod= np.prod(temp)
+            b=(1-params[0])*prod
+            if(a>b):
+                ypred[i]=1
+            else:
+                ypred[i]=2
+
+
+
+
+        predictions= ypred
         return predictions
         
 def evaluateBias(y_pred,y_sensitive):
@@ -81,7 +173,29 @@ def evaluateBias(y_pred,y_sensitive):
     '''
     #remove next line and implement from here
     di = 0
-    
+    count1=0
+    count2 = 0
+    b= 0
+    a=0
+    y_pred=y_pred.flatten()
+
+    for i in range(0,y_pred.shape[0]):
+        if y_pred[i]==2:
+            a+= 1
+            if y_sensitive[i]!=1:
+                 count1+=1
+
+    for i in range(0,y_pred.shape[0]):
+        if y_pred[i]==2:
+            b+= 1
+            if y_sensitive[i]==1:
+                count2+=1
+
+    num = a/count1
+    den = b/count2
+    print(num)
+    print(den)
+    di= (num)/(den)
     #do not change the line below
     return di
 
